619216bc06d6b01585f67306181880c499775f49
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-12-11 08:31:22 +0600

    pagination calculate utility done

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,3 +1,6 @@
+import { SortOrder } from 'mongoose';
+import { paginationHelper } from '../../../helpers/paginationHelper';
+import { IPaginationOptions } from '../../../interface/pagination';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.modal';
 
@@ -8,9 +11,29 @@ const createSemester = async (
   return result;
 };
 
-const getAllSemester = async () => {
-  const result = await AcademicSemester.find({});
-  return result;
+const getAllSemester = async (paginationOptions: IPaginationOptions) => {
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(paginationOptions);
+  console.log('🚀 ~ getAllSemester ~ sortBy:', sortBy, sortOrder);
+
+  const sortCondition: { [kay: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortCondition[sortBy] = sortOrder;
+  }
+  const result = await AcademicSemester.find({})
+    .sort(sortCondition)
+    .skip(skip)
+    .limit(limit);
+  const total = await AcademicSemester.countDocuments();
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
 export const AcademicSemesterService = {
   createSemester,
