191cd407a5564bdb702130f6052790c5fac213f4
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-01-27 23:27:54 +0600

    student interface  gerdian define defined

--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -3,6 +3,22 @@ type UserName = {
   middleName: string;
   lastName: string;
 };
+export type Guardian = {
+    fatherName: string;
+    fatherOccupation: string;
+    fatherContactNo: string;
+    motherName: string;
+    motherOccupation: string;
+    motherContactNo: string;
+    address: string;
+  };
+  
+  export type LocalGuardian = {
+    name: string;
+    occupation: string;
+    contactNo: string;
+    address: string;
+  };
 export type IStudent = {
   id: string;
   name: UserName;
@@ -14,4 +30,5 @@ export type IStudent = {
   bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
   presendAdress: string;
   permanentAddress: string;
+  gurdian: 
 };
