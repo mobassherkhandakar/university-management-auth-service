356d0885c5c691cc372b23b0d1f3a2481c71ea5a
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-02-18 03:28:51 +0600

    user interface modified

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,5 +1,11 @@
+import { Types } from 'mongoose';
+
 export type IUser = {
   id: string;
   role: string;
   password: string;
+  needPasswordChange: boolean;
+  student?: Types.ObjectId;
+  faculty?: Types.ObjectId;
+  admin?: Types.ObjectId;
 };
