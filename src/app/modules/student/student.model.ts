deeedec105814c0364853d1e46301fc368222e16
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-03-09 03:34:30 +0600

    student model toJson added

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -2,138 +2,143 @@ import { Schema, model } from 'mongoose';
 import { gender } from './student.constant';
 import { IStudent } from './student.interface';
 
-const StudentSchema = new Schema<IStudent>({
-  id: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: {
-      firstName: {
-        type: String,
-        required: true,
+const StudentSchema = new Schema<IStudent>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: {
+        firstName: {
+          type: String,
+          required: true,
+        },
+        middleName: {
+          type: String,
+          required: true,
+        },
+        lastName: {
+          type: String,
+          required: true,
+        },
       },
-      middleName: {
-        type: String,
-        required: true,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: gender,
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      required: true,
+    },
+    presendAdress: {
+      type: String,
+      required: true,
+    },
+    permanentAddress: {
+      type: String,
+      required: true,
+    },
+    guardian: {
+      type: {
+        fatherName: {
+          type: String,
+          required: true,
+        },
+        fatherOccupation: {
+          type: String,
+          required: true,
+        },
+        fatherContactNo: {
+          type: String,
+          required: true,
+        },
+        motherName: {
+          type: String,
+          required: true,
+        },
+        motherOccupation: {
+          type: String,
+          required: true,
+        },
+        motherContactNo: {
+          type: String,
+          required: true,
+        },
+        address: {
+          type: String,
+          required: true,
+        },
       },
-      lastName: {
-        type: String,
-        required: true,
+      required: true,
+    },
+    localGuardian: {
+      required: true,
+      type: {
+        name: {
+          type: String,
+          required: true,
+        },
+        occupation: {
+          type: String,
+          required: true,
+        },
+        contactNo: {
+          type: String,
+          required: true,
+        },
+        address: {
+          type: String,
+          required: true,
+        },
       },
     },
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: gender,
-  },
-  dateOfBirth: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  contactNo: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  emergencyContactNo: {
-    type: String,
-    required: true,
-  },
-  bloodGroup: {
-    type: String,
-    required: true,
-  },
-  presendAdress: {
-    type: String,
-    required: true,
-  },
-  permanentAddress: {
-    type: String,
-    required: true,
-  },
-  guardian: {
-    type: {
-      fatherName: {
-        type: String,
-        required: true,
-      },
-      fatherOccupation: {
-        type: String,
-        required: true,
-      },
-      fatherContactNo: {
-        type: String,
-        required: true,
-      },
-      motherName: {
-        type: String,
-        required: true,
-      },
-      motherOccupation: {
-        type: String,
-        required: true,
-      },
-      motherContactNo: {
-        type: String,
-        required: true,
-      },
-      address: {
-        type: String,
-        required: true,
-      },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
     },
-    required: true,
-  },
-  localGuardian: {
-    required: true,
-    type: {
-      name: {
-        type: String,
-        required: true,
-      },
-      occupation: {
-        type: String,
-        required: true,
-      },
-      contactNo: {
-        type: String,
-        required: true,
-      },
-      address: {
-        type: String,
-        required: true,
-      },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+      required: true,
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicSemester',
+      required: true,
+    },
+    profileImage: {
+      type: String,
     },
   },
-  academicFaculty: {
-    type: Schema.Types.ObjectId,
-    ref: 'AcademicFaculty',
-    required: true,
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
   },
-  academicDepartment: {
-    type: Schema.Types.ObjectId,
-    ref: 'AcademicDepartment',
-    required: true,
-  },
-  academicSemester: {
-    type: Schema.Types.ObjectId,
-    ref: 'AcademicSemester',
-    required: true,
-  },
-  profileImage: {
-    type: String,
-  },
-}
-{
-    timestamps: true
-});
+);
 
 export const Student = model<IStudent>('Student', StudentSchema);
