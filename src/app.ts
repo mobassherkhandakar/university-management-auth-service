5c8315b932f9ee919a94f94376978f2ed10f5090
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-03-15 20:58:51 +0600

    [done] setupt mongose cors perser express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express, { Application } from 'express'
+import cors from 'cors'
+const app:Application = express()
+
+app.use(cors())
+
+//perser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
