d66e43c2a381a61c869decb0e708d32fb2ae6d15
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-03-16 02:05:50 +0600

    lint test done

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log('Database is conncected successfully');
-        app.listen(config.port, () => {
-            console.log(`Application app listening on port ${config.port}`)
-          })
-    }catch(error){
-        console.log("Faild to connect database connection",error);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database is conncected successfully')
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Faild to connect database connection', error)
+  }
 }
 bootstrap()
