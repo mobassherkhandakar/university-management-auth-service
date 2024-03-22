9d1fef5e3d48855bbf6f961710f480722d453990
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-03-22 15:53:18 +0600

    IAcademicSemester interface done

---
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -0,0 +1,24 @@
+export type IAcademicSemesterMonth =
+  | 'January'
+  | 'February'
+  | 'March'
+  | 'April'
+  | 'May'
+  | 'June'
+  | 'July'
+  | 'August'
+  | 'September'
+  | 'October'
+  | 'November'
+  | 'December';
+
+export type IAcademicSemesterCode = '01' | '02' | '03';
+export type IAcademicSemesterTitle = 'Autumn' | 'Summer' | 'Fall';
+
+export type IAcademicSemester = {
+  title: IAcademicSemesterTitle;
+  year: string;
+  code: IAcademicSemesterCode;
+  startMonth: IAcademicSemesterMonth;
+  endMonth: IAcademicSemesterMonth;
+};
