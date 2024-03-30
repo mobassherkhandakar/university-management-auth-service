0a5500d9fc13b464acef76c595de2cda76e5c1de
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2024-03-31 00:36:14 +0600

    mangagement department create done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { AcademicDepartmentRoute } from '../modules/academicDepertment/academicDepertment.routes';
 import { AcademicFacultyRoute } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemesterRoute } from '../modules/academicSemester/academicSemester.routes';
+import { ManagementDepartmentRoute } from '../modules/managementDepartment/managementDepartment.route';
 import { UserRouter } from '../modules/user/user.router';
 
 const routes = express.Router();
@@ -22,6 +23,10 @@ const moduleRoutes = [
   {
     path: '/academic-department',
     route: AcademicDepartmentRoute,
+  },
+  {
+    path: '/management-departments',
+    route: ManagementDepartmentRoute,
   },
 ];
 moduleRoutes.forEach(route => routes.use(route.path, route.route));
